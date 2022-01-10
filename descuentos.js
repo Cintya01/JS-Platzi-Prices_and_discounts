7a5611478836756973b165d8fbf6e5a28339fdc4
Cintya01 <cintya.olivares.c@gmail.com>
2022-01-09 22:57:41 -0300

    JS Listo

---
+++ descuentos.js
@@ -0,0 +1,32 @@
+const precioOriginal = 120;
+const descuento = 18;
+
+function calcularPrecioConDescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+    return precioConDescuento;
+}
+
+function finalPriceBtn(){
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+
+
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+
+    const FinalPrice= document.getElementById("finalPrice");
+    FinalPrice.innerText = "El precio con descuento es: $" + precioConDescuento;
+}
+
+
+
+// console.log({
+//     precioOriginal,
+//     descuento,
+//     porcentajePrecioConDescuento,
+//     precioConDescuento
+// });
+
